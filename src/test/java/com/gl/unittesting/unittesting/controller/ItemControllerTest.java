9b364739a8e0584498e29f1bba04906d9e18692d
thaneshwar sahu <thaneshwar.sahu@globallogic.com>
2019-12-25 09:25:42 +0530

    unit testing ItemController and basic json asserstion

---
+++ src/test/java/com/gl/unittesting/unittesting/controller/ItemControllerTest.java
@@ -0,0 +1,41 @@
+package com.gl.unittesting.unittesting.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockReset;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(value = ItemController.class)
+@ExtendWith(value = SpringExtension.class)
+class ItemControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    void dummyItem() throws Exception {
+        //call Get "/dummy-item" application/json
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/dummy-item")
+                .accept(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"id\": 1,\"name\":\"Push Ball\",\"price\":300,\"quantity\":3}"))
+                .andReturn();
+
+        //verify the result
+        //assertEquals();
+    }
+}
